ab690c11ac068d835c2a47c66f7fe41641a06e60
jonzin78 <jonathannyaguii@gmail.com>
2026-01-07 10:31:37 +0900

    0106

--- JS/news.js
+++ JS/news.js
@@ -8,30 +8,40 @@ const client = createClient({
   apiKey: "sBsiDsclPsMlxYJE1OVQk0AD9fiZSBct3ubM",
 });
 
-// ★ news の一覧を取得
+// news一覧を取得
 client
-  .get({
-    endpoint: "news",
-  })
+  .get({ endpoint: "news" })
   .then((res) => {
-    const wrap = document.getElementById("news-list");
+    console.log(res.contents); // ★ デバッグ用（確認できたら消してOK）
 
-    // データ（res.contents）を並べて表示
+    const wrap = document.getElementById("news-list");
+    if (!wrap) return;
+
     wrap.innerHTML = res.contents
-      .map(
-        (item) => `
-      <a href="./news-page.html?id=${item.id}">
-        <div class="news-page">
-          <img class="news-image" src="${item.image?.url || "./images/church_in.JPG"}" alt="">
-          <div class="news-right">
-            <p class="news-page-date">${item.publishedAt?.substring(0, 10)}</p>
-            <p class="news-page-text">${item.title}</p>
-          </div>
-          <img class="btn-more" src="./images/btn-more.svg" alt="">
-        </div>
-      </a>
-    `
-      )
+      .map((item) => {
+        const imageUrl = item.thumbnail?.url
+          ? item.thumbnail.url
+          : "./images/church_in.JPG";
+
+        const date = item.publishedAt
+          ? item.publishedAt.substring(0, 10)
+          : "";
+
+        return `
+          <a href="./news-page.html?id=${item.id}">
+            <div class="news-page">
+              <img class="news-image" src="${imageUrl}" alt="${item.title}">
+              <div class="news-right">
+                <p class="news-page-date">${date}</p>
+                <p class="news-page-text">${item.title}</p>
+              </div>
+              <img class="btn-more" src="./images/btn-more.svg" alt="">
+            </div>
+          </a>
+        `;
+      })
       .join("");
   })
-  .catch((err) => console.error(err));
+  .catch((err) => {
+    console.error("microCMS error:", err);
+  });
